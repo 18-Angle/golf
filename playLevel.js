35aaf5a5b5fb5199dad6f7385dd811ee1560f5cc
efhiii <efhiii2000@gmail.com>
2020-08-18 11:24:41 -0700

    only go to hole select from hole

--- playLevel.js
+++ playLevel.js
@@ -14,19 +14,19 @@ function s3(tx, ty) {
   switch (ARType) {
     case (1):
       if(h / w > 1.4) {
-        button(0, (h - min) * 0.25 - 0.1 * w, 0.2 * w, 0.2 * w, () => { sb = 1 }, backMini, backMinib);
+        button(0, (h - min) * 0.25 - 0.1 * w, 0.2 * w, 0.2 * w, () => { sb = 2 }, backMini, backMinib);
       } else {
-        button(0, 0, 0.5 * (h - min), 0.5 * (h - min), () => { sb = 1 }, backMini, backMinib);
+        button(0, 0, 0.5 * (h - min), 0.5 * (h - min), () => { sb = 2 }, backMini, backMinib);
       }
       break;
     case (2):
-      button(0, 0, 0.14 * h, 0.14 * h, () => { sb = 1 }, backMini, backMinib);
+      button(0, 0, 0.14 * h, 0.14 * h, () => { sb = 2 }, backMini, backMinib);
       break;
     case (3):
       if(w / h > 2.6) {
-        button(0.5 * w - h, 0, 0.5 * h, 0.2 * h, () => { sb = 1 }, back, backb);
+        button(0.5 * w - h, 0, 0.5 * h, 0.2 * h, () => { sb = 2 }, back, backb);
       } else {
-        button(0, 0, 0.2 * w, 0.08 * w, () => { sb = 1 }, back, backb);
+        button(0, 0, 0.2 * w, 0.08 * w, () => { sb = 2 }, back, backb);
       }
       break;
   }
